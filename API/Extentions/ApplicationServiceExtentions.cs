7190cf4f361036a23fe8c56d8b0fc15f713d1e35
Steve Neiman <tneimandev@gmail.com>
2024-09-29 11:16:13 -0400

    Creating a CRUD application using the CQRS + Mediator pattern

---
+++ API/Extentions/ApplicationServiceExtentions.cs
@@ -0,0 +1,34 @@
+using Application.Core;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using Application.Activities;
+
+
+namespace API.Extentions
+{
+    public static class ApplicationServiceExtentions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddDbContext<DataContext>(opt =>
+             {
+                 opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+             });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
+
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(List.Handler).Assembly));
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+            return services;
+
+        }
+    }
+}
