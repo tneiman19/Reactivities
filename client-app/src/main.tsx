41650f13f4e6e9e1e8a08afedb2dda9fbe401c39
Steve Neiman <tneimandev@gmail.com>
2024-09-29 21:00:14 -0400

    Creating a CRUD application in React

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./app/layout/App";
 import "semantic-ui-css/semantic.min.css";
-import './index.css'
+import './app/layout/styles.css'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
