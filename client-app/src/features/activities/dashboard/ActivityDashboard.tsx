a22c0fa5600cf088162bb59b08c3ae070c837467
Steve Neiman <tneimandev@gmail.com>
2024-10-15 22:19:58 -0400

    Style/Layout User Interface

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,13 +1,14 @@
-import { Grid } from 'semantic-ui-react';
+import {Grid} from 'semantic-ui-react';
 import ActivityList from './ActivityList';
-import { useStore } from '../../../app/stores/store';
-import { observer } from 'mobx-react-lite';
-import { useEffect } from 'react';
+import {useStore} from '../../../app/stores/store';
+import {observer} from 'mobx-react-lite';
+import {useEffect} from 'react';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
+import ActivityFilters from './ActivityFilters';
 
 export default observer(function ActivityDashboard() {
-  const { activityStore } = useStore();
-  const { loadActivities, activityRegistry } = activityStore;
+  const {activityStore} = useStore();
+  const {loadActivities, activityRegistry} = activityStore;
 
   useEffect(() => {
     if (activityRegistry.size <= 1) loadActivities();
@@ -22,7 +23,7 @@ export default observer(function ActivityDashboard() {
           <ActivityList />
         </Grid.Column>
         <Grid.Column width='6'>
-          <h2>Activity filters</h2>
+          <ActivityFilters />
         </Grid.Column>
       </Grid>
     </>
--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 import 'semantic-ui-css/semantic.min.css';
+import 'react-calendar/dist/Calendar.css';
 import './app/layout/styles.css';
-import { StoreContext, store } from './app/stores/store';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './app/router/Routes';
+import {StoreContext, store} from './app/stores/store';
+import {RouterProvider} from 'react-router-dom';
+import {router} from './app/router/Routes';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
