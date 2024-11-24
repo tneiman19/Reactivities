439e934343c127af360b2f9bd8d6fcd864c837f7
Steve Neiman <tneimandev@gmail.com>
2024-11-24 15:22:26 -0500

    Client image upload | Section 17

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -53,4 +53,8 @@ export default class UserStore {
       console.error(error);
     }
   };
+
+  setImage = (image: string) => {
+    if (this.user) this.user.image = image;
+  };
 }
---
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -0,0 +1,30 @@
+import {observer} from "mobx-react-lite";
+import {Tab, TabPane} from "semantic-ui-react";
+import {Profile} from "../../app/models/profile";
+import ProfilePhotos from "./ProfilePhotos";
+
+interface Props {
+  profile: Profile;
+}
+export default observer(function ProfileContent({profile}: Props) {
+  const panes = [
+    {menuItem: "About", render: () => <TabPane>About Content</TabPane>},
+    {menuItem: "Photos", render: () => <ProfilePhotos profile={profile} />},
+    {menuItem: "Events", render: () => <TabPane>Events Content</TabPane>},
+    {
+      menuItem: "Followers",
+      render: () => <TabPane>Followers Content</TabPane>,
+    },
+    {
+      menuItem: "Following",
+      render: () => <TabPane>Following Content</TabPane>,
+    },
+  ];
+  return (
+    <Tab
+      menu={{fluid: true, vertical: true}}
+      menuPosition='right'
+      panes={panes}
+    />
+  );
+});
